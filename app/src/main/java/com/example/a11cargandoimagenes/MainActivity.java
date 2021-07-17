629a22fa37a393e6e905798e20334644b0d9ee00
MIGUEL BEASCOA <77923795+mbeascoa@users.noreply.github.com>
2021-07-17 20:53:41 +0200

    Initial commit

---
+++ app/src/main/java/com/example/a11cargandoimagenes/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.a11cargandoimagenes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView logo = (ImageView) findViewById(R.id.imageView1);
+        logo.setImageResource(R.drawable.shrek);
+
+    }
+}
